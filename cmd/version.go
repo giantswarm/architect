bf854cebab43b953c6bba097d8a30594be2fc5ca
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-02-17 10:10:05 +0000

    General late night improvements (#13)
    * Fixes missing server argument
    
    * Adds version command

---
+++ cmd/version.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "show version information",
+		Run:   runVersion,
+	}
+
+	Commit         string
+	BuildTimestamp string
+)
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
+
+func runVersion(cmd *cobra.Command, args []string) {
+	if Commit == "" && BuildTimestamp == "" {
+		fmt.Printf("version information not compiled\n")
+		os.Exit(-1)
+	}
+
+	fmt.Printf("Git Commit Hash: %s\n", Commit)
+	fmt.Printf("Build Timestamp: %s\n", BuildTimestamp)
+}
