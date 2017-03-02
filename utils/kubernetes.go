8f0612eaaf790a9fd7b301b5aa15ea100651d162
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-03-02 11:05:58 +0000

    Adds support for fetching kubernetes certificates from environment (#14)
    When running in circle, we currently provide kubernetes
    certificates via env vars.
    This changeset lets us automatically fetch them,
    which makes the circle.yml simpler

--- utils/kubernetes.go
+++ utils/kubernetes.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/base64"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -33,4 +35,42 @@ func TemplateKubernetesResources(kubernetesResourcesDirectoryPath, templatedReso
 	}
 
 	return nil
+}
+
+// FetchKubernetesCertsFromEnvironment attempts to load certificates from the environment
+func FetchKubernetesCertsFromEnvironment(workingDirectory string) (string, string, string, error) {
+	caName := "ca.pem"
+	crtName := "crt.pem"
+	keyName := "key.pem"
+
+	assets := []struct {
+		envVar string
+		name   string
+	}{
+		{envVar: "G8S_CA", name: caName},
+		{envVar: "G8S_CRT", name: crtName},
+		{envVar: "G8S_KEY", name: keyName},
+	}
+
+	for _, asset := range assets {
+		encodedData := os.Getenv(asset.envVar)
+		if encodedData == "" {
+			return "", "", "", fmt.Errorf("could not load certificate data from %v\n", asset.envVar)
+		}
+
+		data, err := base64.StdEncoding.DecodeString(encodedData)
+		if err != nil {
+			return "", "", "", err
+		}
+
+		if err := ioutil.WriteFile(asset.name, data, 0644); err != nil {
+			return "", "", "", err
+		}
+	}
+
+	caPath := filepath.Join(workingDirectory, caName)
+	crtPath := filepath.Join(workingDirectory, crtName)
+	keyPath := filepath.Join(workingDirectory, keyName)
+
+	return caPath, crtPath, keyPath, nil
 }
