083809df7540b7a24fee7685a5b5bac2f4732922
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-03-14 13:03:41 +0000

    Adds support for deploying to multiple clusters (#33)
    * Adds support for deploying to multiple clusters
    
    * Refactor to use clusters from env in deploy
    
    * Updates AWS cluster server endpoint

--- utils/kubernetes.go
+++ utils/kubernetes.go
@@ -1,10 +1,7 @@
 package utils
 
 import (
-	"encoding/base64"
-	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/spf13/afero"
@@ -47,42 +44,4 @@ func TemplateKubernetesResources(fs afero.Fs, resourcesDir, sha string) error {
 	}
 
 	return nil
-}
-
-// K8SCertsFromEnv attempts to load certificates from the environment
-func K8SCertsFromEnv(fs afero.Fs, workingDirectory string) (string, string, string, error) {
-	caName := "ca.pem"
-	crtName := "crt.pem"
-	keyName := "key.pem"
-
-	assets := []struct {
-		envVar string
-		name   string
-	}{
-		{envVar: "G8S_CA", name: caName},
-		{envVar: "G8S_CRT", name: crtName},
-		{envVar: "G8S_KEY", name: keyName},
-	}
-
-	for _, asset := range assets {
-		encodedData := os.Getenv(asset.envVar)
-		if encodedData == "" {
-			return "", "", "", fmt.Errorf("could not load certificate data from %v\n", asset.envVar)
-		}
-
-		data, err := base64.StdEncoding.DecodeString(encodedData)
-		if err != nil {
-			return "", "", "", err
-		}
-
-		if err := afero.WriteFile(fs, asset.name, data, 0644); err != nil {
-			return "", "", "", err
-		}
-	}
-
-	caPath := filepath.Join(workingDirectory, caName)
-	crtPath := filepath.Join(workingDirectory, crtName)
-	keyPath := filepath.Join(workingDirectory, keyName)
-
-	return caPath, crtPath, keyPath, nil
 }
