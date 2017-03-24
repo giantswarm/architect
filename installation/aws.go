d5beb3c321b91491c46515e7d376dfa25ccafdb9
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-03-24 10:57:01 +0000

    Adds aws configuration (#43)

---
+++ installation/aws.go
@@ -0,0 +1,70 @@
+package installation
+
+import (
+	"net/url"
+	"time"
+
+	"github.com/giantswarm/architect/configuration"
+	"github.com/giantswarm/architect/configuration/auth"
+	"github.com/giantswarm/architect/configuration/auth/vault"
+	"github.com/giantswarm/architect/configuration/cluster"
+	"github.com/giantswarm/architect/configuration/cluster/hyperkube"
+	"github.com/giantswarm/architect/configuration/cluster/kubernetes"
+	"github.com/giantswarm/architect/configuration/giantswarm"
+	"github.com/giantswarm/architect/configuration/giantswarm/api"
+	"github.com/giantswarm/architect/configuration/monitoring"
+	"github.com/giantswarm/architect/configuration/monitoring/prometheus"
+	"github.com/giantswarm/architect/configuration/monitoring/testbot"
+)
+
+var AWS = configuration.Installation{
+	V1: configuration.V1{
+		Auth: auth.Auth{
+			Vault: vault.Vault{
+				Address: url.URL{
+					Scheme: "https",
+					Host:   "vault.eu-west-1.aws.private.giantswarm.io:8200",
+				},
+				CA: vault.CA{
+					TTL: 10 * 365 * 24 * time.Hour,
+				},
+				Token: vault.Token{
+					TTL: 30 * 24 * time.Hour,
+				},
+			},
+		},
+
+		GiantSwarm: giantswarm.GiantSwarm{
+			API: api.API{
+				Address: url.URL{
+					Scheme: "https",
+					Host:   "api-aws.giantswarm.io",
+				},
+			},
+		},
+
+		Guest: cluster.Guest{
+			Hyperkube: hyperkube.Hyperkube{
+				Version: "v1.5.2_coreos.0",
+			},
+			Kubernetes: kubernetes.Kubernetes{
+				API: kubernetes.API{
+					EndpointBase: "g8s.eu-west-1.aws.adidas.private.giantswarm.io",
+				},
+			},
+		},
+
+		Monitoring: monitoring.Monitoring{
+			Prometheus: prometheus.Prometheus{
+				Address: url.URL{
+					Scheme: "https",
+					Host:   "prometheus-aws.giantswarm.io",
+				},
+				RetentionPeriod: 2 * 7 * 24 * time.Hour,
+			},
+			Testbot: testbot.Testbot{
+				Interval: 5 * time.Minute,
+			},
+		},
+	},
+}
