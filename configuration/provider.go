c7c137126c57407d14cdd3d5ae93a0aeeb7d69a9
Tim Schindler <xh3b4sd@gmail.com>
2017-04-26 15:22:01 +0200

    godoc

--- configuration/provider.go
+++ configuration/provider.go
@@ -1,8 +1,15 @@
 package configuration
 
+// provider is a private type to ensure only providers defined in this package
+// can be applied to installation configurations. That prevents other packages
+// screwing around with provider configurations.
 type provider string
 
 const (
+	// ProviderAWS represents a G8S installation being deployed on AWS where EC2
+	// provides the virtualization.
 	ProviderAWS provider = "aws"
+	// ProviderAWS represents a G8S installation being deployed on bare metal
+	// where KVM provides the virtualization.
 	ProviderKVM provider = "kvm"
 )
