f5e33feda2c0c0ff286313f0579d1309b1b4bba7
Tim Schindler <xh3b4sd@gmail.com>
2017-04-27 19:06:25 +0200

    fixed guest cluster structure (#58)

--- configuration/cluster/hyperkube/hyperkube.go
+++
@@ -1,9 +0,0 @@
-// Package hyperkube provides configuration structures for Hyperkube.
-package hyperkube
-
-// Hyperkube holds configuration for Hyperkube settings.
-type Hyperkube struct {
-	// Version is the version of Hyperkube.
-	// e.g: 'v1.5.2_coreos.0'
-	Version string
-}
--- installation/leaseweb.go
+++ installation/leaseweb.go
@@ -7,14 +7,14 @@ import (
 	"github.com/giantswarm/architect/configuration"
 	"github.com/giantswarm/architect/configuration/auth"
 	"github.com/giantswarm/architect/configuration/auth/vault"
-	"github.com/giantswarm/architect/configuration/cluster"
-	"github.com/giantswarm/architect/configuration/cluster/hyperkube"
-	"github.com/giantswarm/architect/configuration/cluster/kubernetes"
 	"github.com/giantswarm/architect/configuration/giantswarm"
 	"github.com/giantswarm/architect/configuration/giantswarm/api"
 	"github.com/giantswarm/architect/configuration/giantswarm/desmotes"
 	"github.com/giantswarm/architect/configuration/giantswarm/happa"
 	"github.com/giantswarm/architect/configuration/giantswarm/passage"
+	"github.com/giantswarm/architect/configuration/guest"
+	"github.com/giantswarm/architect/configuration/guest/hyperkube"
+	"github.com/giantswarm/architect/configuration/guest/kubernetes"
 	"github.com/giantswarm/architect/configuration/monitoring"
 	"github.com/giantswarm/architect/configuration/monitoring/prometheus"
 	"github.com/giantswarm/architect/configuration/monitoring/testbot"
@@ -66,9 +66,9 @@ var Leaseweb = configuration.Installation{
 			},
 		},
 
-		Guest: cluster.Guest{
+		Guest: guest.Guest{
 			Hyperkube: hyperkube.Hyperkube{
-				Version: "v1.5.2_coreos.0",
+				Version: hyperkube.Version,
 			},
 			Kubernetes: kubernetes.Kubernetes{
 				API: kubernetes.API{
