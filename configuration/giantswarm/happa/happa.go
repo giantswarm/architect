5f20f2f5d3c7f2fa6568df66b5de6eb04f1c0e9c
Tim Schindler <xh3b4sd@gmail.com>
2017-04-28 12:00:34 +0200

    removed unused cluster worker type from happa config

--- configuration/giantswarm/happa/happa.go
+++ configuration/giantswarm/happa/happa.go
@@ -10,8 +10,4 @@ type Happa struct {
 	// Address is the URL to Happa.
 	// e.g: 'https://happa-g8s.giantswarm.io'
 	Address url.URL
-
-	// CreateClusterWorkerType controls the type of form to show when creating a
-	// cluster. Valid values are 'aws' or 'kvm'.
-	CreateClusterWorkerType string
 }
