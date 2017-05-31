8854a98c14694857c6d4056e87bad5f729f95b8b
Ross Fairbanks <rossf7@users.noreply.github.com>
2017-05-31 17:40:14 +0200

    Adds Route 53 settings to AWS provider (#92)
    * Add Route 53 section to the AWS provider for hosted zones
    
    * Set values for Hosted Zone IDs in the AWS installation
    
    * Fix import
    
    * Fix import 2

--- configuration/provider/aws/aws.go
+++ configuration/provider/aws/aws.go
@@ -3,10 +3,13 @@ package aws
 
 import (
 	"github.com/giantswarm/architect/configuration/provider/aws/ec2"
+	"github.com/giantswarm/architect/configuration/provider/aws/route53"
 )
 
 // AWS holds configuration for the AWS provider.
 type AWS struct {
 	// EC2 holds configuration for the EC2 specific settings.
 	ec2.EC2
+	// Route53 holds configuration for the Route53 specific settings.
+	route53.Route53
 }
