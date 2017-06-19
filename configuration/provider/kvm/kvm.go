de4978a19a526c94f27034101d66b4b984f760b2
Tim Schindler <xh3b4sd@gmail.com>
2017-06-19 12:55:13 +0200

    added port range for KVM Leaseweb (#102)

--- configuration/provider/kvm/kvm.go
+++ configuration/provider/kvm/kvm.go
@@ -1,7 +1,11 @@
 package kvm
 
-import "github.com/giantswarm/architect/configuration/provider/kvm/flannel"
+import (
+	"github.com/giantswarm/architect/configuration/provider/kvm/flannel"
+	"github.com/giantswarm/architect/configuration/provider/kvm/ingress"
+)
 
 type KVM struct {
 	Flannel flannel.Flannel
+	Ingress ingress.Ingress
 }
