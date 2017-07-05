4c97d165a92ef8c21f2cc672d1a71f6d892efd85
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-07-05 12:45:28 +0200

    godoc standart fix

--- configuration/guest/calico/calico.go
+++ configuration/guest/calico/calico.go
@@ -2,10 +2,8 @@ package calico // Package calico provides configuration structures for Calico.
 
 // Calico holds configuration for guest cluster calico settings.
 type Calico struct {
-	// subnet for calico
-	// ie 192.168.0.0
+	// Subnet for calico, eg. 192.168.0.0.
 	Subnet string
-	// cidr for calico sibnets
-	// ie 16
+	// Cidr for calico subnets, eg. 16.
 	CIDR string
 }
