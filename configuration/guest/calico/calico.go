e01a8df948c82701af8d3d3308ee1f030d4aa22a
Vaclav Rozsypalek <calvin_24@seznam.cz>
2017-07-05 12:52:49 +0200

    customizable-networks (#112)
    * customizable-networks
    
    * godoc standart fix
    
    * godoc standart fix2

---
+++ configuration/guest/calico/calico.go
@@ -0,0 +1,9 @@
+package calico // Package calico provides configuration structures for Calico.
+
+// Calico holds configuration for guest cluster calico settings.
+type Calico struct {
+	// Subnet for calico, eg. 192.168.0.0.
+	Subnet string
+	// CIDR for calico subnets, eg. 16.
+	CIDR string
+}
