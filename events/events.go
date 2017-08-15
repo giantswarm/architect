1fe15e82d8da3b22ea2a21c2813bc7b33a31607e
Roman Sokolkov <rsokolkov@gmail.com>
2017-08-15 11:49:44 +0200

    Add ginger (#144)
    This is new GiantSwarm environment
    that will be used for customer playgrounds

--- events/events.go
+++ events/events.go
@@ -165,6 +165,25 @@ var environmentProjects = map[Environment][]string{
 		"tokend",
 		"userd",
 	},
+	"ginger": []string{
+		"api",
+		"cert-operator",
+		"cluster-service",
+		"companyd",
+		"desmotes",
+		"draughtsman",
+		"etcd-backup",
+		"g8s-etcd-operator",
+		"g8s-grafana",
+		"g8s-prometheus",
+		"happa",
+		"kubernetesd",
+		"aws-operator",
+		"passage",
+		"testbot",
+		"tokend",
+		"userd",
+	},
 }
 
 // GetEnvironments takes a project name, and returns a list of environments
