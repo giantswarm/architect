a1139a72ce91564e8cfce1aee912a380f56aabbc
Theo Brigitte <theo.brigitte@gmail.com>
2019-03-06 11:37:44 +0100

    kill releaseInfo and use ReleaseGithubTask

--- release/task.go
+++ release/task.go
@@ -12,33 +12,19 @@ const (
 )
 
 type ReleaseGithubTask struct {
-	Client       *github.Client
-	Dir          string
+	Client *github.Client
+
+	AssetsDir    string
+	Draft        bool
 	Organisation string
 	Project      string
 	Sha          string
 	Tag          string
 }
 
-func NewReleaseGithubTaskclient(client *github.Client) ReleaseGithubTask {
-	task := ReleaseGithubTask{
-		Client: client,
-	}
-
-	return task
-}
-
-// Run creates a draft github release.
+// Run ensures a github release.
 func (r ReleaseGithubTask) Run() error {
-	info := releaseInfo{
-		AssetsDir:    r.Dir,
-		Draft:        true,
-		Organisation: r.Organisation,
-		Project:      r.Project,
-		Sha:          r.Sha,
-		Tag:          r.Tag,
-	}
-	return ensureWithDir(r.Client, info)
+	return r.ensureWithDir()
 }
 
 func (r ReleaseGithubTask) Name() string {
