df6a149b232215639ab3d815968848dd842f5f27
Theo Brigitte <theo.brigitte@gmail.com>
2019-05-10 21:30:40 +0200

    add circleci trigger-job command logic

--- cmd/circleci/triggerjob/runner.go
+++ cmd/circleci/triggerjob/runner.go
@@ -1,9 +1,48 @@
 package triggerjob
 
 import (
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	"github.com/jszwedko/go-circleci"
 	"github.com/spf13/cobra"
 )
 
 func runTriggerJobError(cmd *cobra.Command, args []string) error {
+	var (
+		token  = cmd.Flag("token").Value.String()
+		org    = cmd.Flag("org").Value.String()
+		repo   = cmd.Flag("repo").Value.String()
+		branch = cmd.Flag("branch").Value.String()
+		job    = cmd.Flag("job").Value.String()
+	)
+
+	if org == "" {
+		return microerror.Mask(missingOrganisationError)
+	}
+
+	if repo == "" {
+		return microerror.Mask(missingRepositoryError)
+	}
+
+	if branch == "" {
+		return microerror.Mask(missingBranchError)
+	}
+
+	var params map[string]string = nil
+	if job != "" {
+		params = map[string]string{
+			"CIRCLE_JOB": job,
+		}
+	}
+
+	client := &circleci.Client{Token: token}
+	build, err := client.ParameterizedBuild(org, repo, branch, params)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	fmt.Printf("job triggered: %s\n", build.BuildURL)
+
 	return nil
 }
