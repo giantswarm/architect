37e1210d049100da679f23b39cf1e47cd979720a
Theo Brigitte <theo.brigitte@gmail.com>
2019-05-12 16:05:38 +0200

    move to persistent flags

--- cmd/circleci/triggerjob/flag.go
+++ cmd/circleci/triggerjob/flag.go
@@ -1,9 +1,9 @@
 package triggerjob
 
 func init() {
-	Cmd.LocalFlags().String("branch", "master", "project's branch name")
-	Cmd.LocalFlags().String("job", "", "CircleCI job name. This can only be used for project which do not have a defined workflow.")
-	Cmd.LocalFlags().String("org", "", "CircleCI organization or username")
-	Cmd.LocalFlags().String("project", "", "CircleCI project name")
-	Cmd.LocalFlags().String("token", "", "CircleCI API token")
+	Cmd.PersistentFlags().String("branch", "master", "project's branch name")
+	Cmd.PersistentFlags().String("job", "", "CircleCI job name. This can only be used for project which do not have a defined workflow.")
+	Cmd.PersistentFlags().String("org", "", "CircleCI organization or username")
+	Cmd.PersistentFlags().String("project", "", "CircleCI project name")
+	Cmd.PersistentFlags().String("token", "", "CircleCI API token")
 }
