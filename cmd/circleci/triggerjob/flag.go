46b3be6e34033f4af0592901b91966b1c7b2470f
Theo Brigitte <theo.brigitte@gmail.com>
2019-05-12 15:05:22 +0200

    update flag usage

--- cmd/circleci/triggerjob/flag.go
+++ cmd/circleci/triggerjob/flag.go
@@ -2,8 +2,8 @@ package triggerjob
 
 func init() {
 	Cmd.Flags().String("branch", "master", "name of the repository branch")
-	Cmd.Flags().String("job", "", "CircleCI job")
-	Cmd.Flags().String("org", "", "CircleCI organization")
-	Cmd.Flags().String("repo", "", "CircleCI repository")
+	Cmd.Flags().String("job", "", "CircleCI job name")
+	Cmd.Flags().String("org", "", "CircleCI organization or username")
+	Cmd.Flags().String("repo", "", "CircleCI repository or project name")
 	Cmd.Flags().String("token", "", "CircleCI API token")
 }
