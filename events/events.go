e6654baff35ab3d2da3cee4554aca29318336a93
Marian Steinbach <marian@giantswarm.io>
2019-11-18 23:12:19 +0100

    Removing some apps from gollum_6iec4

--- events/events.go
+++ events/events.go
@@ -95,19 +95,11 @@ var environmentProjects = map[Environment][]string{
 
 	// non standard cluster just for our website
 	"gollum_6iec4": {
-		"api-spec",
 		"auto-oncall",
 		"circleci-exporter",
-		"docs",
-		"docs-indexer",
-		"docs-proxy",
 		"draughtsman",
-		"giantswarmio-nginx",
-		"giantswarmio-webapp",
 		"github-exporter",
 		"kpi-prometheus",
-		"web-assets",
-		"sitesearch",
 	},
 }
 
