55205b38904839b212635584beb465c604425370
Jose Armesto <github@armesto.net>
2020-02-12 14:01:21 +0100

    Unset CIRCLE_BRANCH for testing purposes

--- workflow/workflow_test.go
+++ workflow/workflow_test.go
@@ -2,6 +2,7 @@ package workflow
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"reflect"
 	"strings"
@@ -243,6 +244,9 @@ func TestGetBuildWorkflow(t *testing.T) {
 			setUp: func(fs afero.Fs, testDir string) error {
 				projectInfo.WorkingDirectory = testDir
 
+				if err := os.Setenv("CIRCLE_BRANCH", ""); err != nil {
+					return microerror.Mask(err)
+				}
 				if err := fs.MkdirAll(filepath.Join(projectInfo.WorkingDirectory, "helm", "test-project-chart"), 0744); err != nil {
 					return microerror.Mask(err)
 				}
