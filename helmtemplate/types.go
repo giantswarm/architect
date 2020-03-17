1a54802ef6fd9c91a9e36233cede4435e1aa0f35
wtty-fool <wtty.fool@gmail.com>
2020-03-17 14:39:37 +0100

    make sure the comment actually tells the truth

--- helmtemplate/types.go
+++ helmtemplate/types.go
@@ -18,6 +18,7 @@ type BuildInfo struct {
 	SHA string
 	// Version is the version of the commit being built.
 	Version string
-	// AppVersion is the version read from project.go
+	// AppVersion is the version read from pkg/project/project.go if it
+	// exsitsts or set to the same value as Version otherwise.
 	AppVersion string
 }
