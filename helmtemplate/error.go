c77fd342727608df07756f29dbd022b7d7535e01
wtty-fool <wtty.fool@gmail.com>
2020-03-18 11:32:21 +0100

    add versionValidationError

--- helmtemplate/error.go
+++ helmtemplate/error.go
@@ -4,11 +4,21 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var invalidConfigError = &microerror.Error{
-	Kind: "invalidConfigError",
-}
+var (
+	invalidConfigError = &microerror.Error{
+		Kind: "invalidConfigError",
+	}
+	versionValidationError = &microerror.Error{
+		Kind: "versionValidationError",
+	}
+)
 
-// IsInvalidConfig asserts incorrectValueError.
+// IsInvalidConfig asserts invalidValueError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
+}
+
+// IsVersionValidationError asserts versionValidationError
+func IsVersionValidationError(err error) bool {
+	return microerror.Cause(err) == versionValidationError
 }
