f027b04ea86ba9c0fe6e48f5e5da0bded0e58a46
Paweł Kopiczko <kopiczko@gmail.com>
2020-03-19 14:44:25 +0000

    make linter happy

--- cmd/helm/template/runner.go
+++ cmd/helm/template/runner.go
@@ -50,7 +50,7 @@ func runTemplateError(cmd *cobra.Command, args []string) (err error) {
 			return microerror.Mask(err)
 		}
 
-		appVersion, err = getProjectVersion(ctx, dir)
+		appVersion, err = getProjectVersion(dir)
 		if err != nil {
 			return microerror.Mask(err)
 		}
@@ -87,7 +87,7 @@ func runTemplateError(cmd *cobra.Command, args []string) (err error) {
 // getProjectVersion retrieves version stored in project's Go source code.
 // It looks up the value of variable `version` in `pkg/project/project.go` file
 // on version defined in ref.
-func getProjectVersion(ctx context.Context, repoDir string) (string, error) {
+func getProjectVersion(repoDir string) (string, error) {
 	filePath := "pkg/project/project.go"
 	varName := "version"
 
