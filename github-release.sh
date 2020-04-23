ca2dae35a1ad3d1d40fdf8624c9935a3fd131650
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2020-04-23 11:30:05 +0200

    Fix github release

--- github-release.sh
+++ github-release.sh
@@ -1,44 +1,14 @@
 #!/bin/bash
 
-SHA=$1
+TAG=$1
 PERSONAL_ACCESS_TOKEN=$2
-
-SHORT_SHA=$(echo ${SHA} | head -c 7)
-DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-
-echo "Creating Tag"
-tag_output=$(curl \
-    --request POST \
-    --header "Authorization: token $PERSONAL_ACCESS_TOKEN" \
-    --header "Content-Type: application/json" \
-    --data "{
-        \"tag\": \"${SHA}\",
-        \"message\": \"automated tag for ${SHORT_SHA}\",
-        \"object\": \"${SHA}\",
-        \"type\": \"commit\",
-        \"tagger\": {
-            \"name\": \"taylorbot\",
-            \"email\": \"dev@giantswarm.io\",
-            \"date\": \"${DATE}\"
-        }
-    }" \
-    https://api.github.com/repos/giantswarm/architect/git/tags
-)
-echo $tag_output | jq
 
 echo "Creating GitHub Release"
 release_output=$(curl \
-    --request POST \
+    --request GET \
     --header "Authorization: token ${PERSONAL_ACCESS_TOKEN}" \
     --header "Content-Type: application/json" \
-    --data "{
-        \"tag_name\": \"${SHORT_SHA}\",
-        \"name\": \"${SHORT_SHA}\",
-        \"body\": \"Automated release for ${SHORT_SHA}.\",
-        \"draft\": false,
-        \"prerelease\": false
-    }" \
-    https://api.github.com/repos/giantswarm/architect/releases
+    https://api.github.com/repos/giantswarm/architect/releases/tags/$TAG
 )
 echo $release_output | jq
 
