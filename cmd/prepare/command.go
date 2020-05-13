9d223a125f4c5ffbe9f788e85ae34651aa653a72
Paweł Kopiczko <pawel@giantswarm.io>
2020-05-13 11:40:09 +0100

    remove hook.PreRunE ; fix cwd (#427)
    * panic
    
    * linter
    
    * use cwd
    
    * panic more
    
    * remove hook
    
    * Update pre_run_e.go
    
    * Update runner.go

--- cmd/prepare/command.go
+++ cmd/prepare/command.go
@@ -2,15 +2,12 @@ package prepare
 
 import (
 	"github.com/spf13/cobra"
-
-	"github.com/giantswarm/architect/cmd/hook"
 )
 
 var (
 	Cmd = &cobra.Command{
-		Use:     "prepare-release",
-		Short:   "prepare changelog and operator version to be released",
-		RunE:    runPrepareRelease,
-		PreRunE: hook.PreRunE,
+		Use:   "prepare-release",
+		Short: "prepare changelog and operator version to be released",
+		RunE:  runPrepareRelease,
 	}
 )
